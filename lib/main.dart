fac8b9b8616172e2f79078be88ba79eb38b4d842
andreasblomqvist <adde-16@hotmail.com>
2019-06-24 22:24:08 +0200

    Working app, EdgeInsets

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.purple,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            //margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
+            margin: EdgeInsets.fromLTRB(30.0, 10.0, 50.0, 20.0),
+            color: Colors.white,
+            child: Text('Hello'),
+          ),
+        ),
       ),
     );
   }
