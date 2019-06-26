764c64035ae4479b4b99fabf07f4354d201c3121
andreasblomqvist <adde-16@hotmail.com>
2019-06-26 22:33:21 +0200

    Code Challenge Completed

--- lib/main.dart
+++ lib/main.dart
@@ -9,32 +9,43 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              height: 100.0,
-              width: 100.0,
-              color: Colors.white,
-              child: Text('Container 1'),
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                // D60620 B70316 950217 7E0315
+                Container(
+                  color: Colors.red,
+                  margin: EdgeInsets.symmetric(vertical: 30),
+                  width: 100,
+                ),
+                Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        color: Colors.yellow,
+                        height: 100,
+                        width: 100,
+                      ),
+                      Container(
+                        color: Colors.green,
+                        height: 100,
+                        width: 100,
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.blue,
+                  margin: EdgeInsets.symmetric(vertical: 30),
+                  width: 100,
+                ),
+              ],
             ),
-            Container(
-              height: 100.0,
-              width: 100.0,
-              color: Colors.red,
-              child: Text('Container 2'),
-            ),
-            Container(
-              height: 100.0,
-              width: 100.0,
-              color: Colors.blue,
-              child: Text('Container 3'),
-            ),
-          ],
-        )),
-      ),
+          )),
     );
   }
 }
